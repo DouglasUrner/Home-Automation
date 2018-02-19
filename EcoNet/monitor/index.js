fb15fabd1634bffd4e7a22dcecbfe46766c1acbb
Douglas Urner <dlu@canishe.com>
2018-02-19 01:10:44 -0800

    Clean up some IntelliJ warnings.

--- EcoNet/monitor/index.js
+++ EcoNet/monitor/index.js
@@ -1,12 +1,12 @@
-const fetch = require('node-fetch')
-const moment = require('moment')
+const fetch = require('node-fetch');
+const moment = require('moment');
 
-var host = 'econet-api.rheemcert.com'
+const host = 'econet-api.rheemcert.com';
 
 // Get credentials
-var fs = require('fs')
-var path = process.cwd()
-var credentials = JSON.parse(fs.readFileSync(path + '/credentials.json'))
+const fs = require('fs');
+const path = process.cwd();
+const credentials = JSON.parse(fs.readFileSync(path + '/credentials.json'));
 
 async function logStatus() {
 
@@ -19,31 +19,31 @@ async function logStatus() {
     },
       body: 'username=' + credentials.username
       + '&password=' + credentials.password + '&grant_type=password'
-  })).json()
+  })).json();
 
-  const token = loginRes.access_token
+  const token = loginRes.access_token;
 
   const equipRes = await (await fetch('https://' + host + '/equipment/' + credentials.id, {
     headers: {
       Accept: 'application/json, text/plain, */*',
       Authorization: 'Bearer ' + token
     },
-  })).json()
+  })).json();
 
-  let log = moment().format()
-  log += " - "
-  log += equipRes.mode
-  log += " - lower " + equipRes.lowerTemp.toFixed(2)
-  log += " - upper " + equipRes.upperTemp.toFixed(2)
-  log += " - ambient " + equipRes.ambientTemp.toFixed(2)
-  log += " - "
-  ;(["isLoadShiftOpted", "isLoadShedOpted", "isLoadShedActive", "isLoadShiftActive", "isEnabled", "isConnected", "isOnVacation", "hasCriticalAlert", "inUse"]).forEach(flag => {
+  let log = moment().format();
+  log += " - ";
+  log += equipRes.mode;
+  log += " - lower " + equipRes.lowerTemp.toFixed(2);
+  log += " - upper " + equipRes.upperTemp.toFixed(2);
+  log += " - ambient " + equipRes.ambientTemp.toFixed(2);
+  log += " - ";
+  (["isLoadShiftOpted", "isLoadShedOpted", "isLoadShedActive", "isLoadShiftActive", "isEnabled", "isConnected", "isOnVacation", "hasCriticalAlert", "inUse"]).forEach(flag => {
     if (equipRes[flag]) {
       log += flag + ' '
     }
-  })
+  });
   console.log(log)
 }
 
-logStatus()
-setInterval(logStatus, 5 * 60 * 1000)
+logStatus();
+setInterval(logStatus, 5 * 60 * 1000);
